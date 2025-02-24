ea36505156d5cfaad248cebe78db181e6b7925d2
Henrique Fonseca <henriquefons@hotmail.com>
2025-02-23 21:43:02 -0300

    fix: remove container from pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -26,49 +26,47 @@ scr
 
 <template>
   <v-form @submit.prevent="signIn">
-    <v-container>
-      <v-row justify="center">
-        <v-col cols="12" md="4">
-          <v-text-field
-            id="email"
-            v-model="user.email"
-            :readonly="authStore.loading.login"
-            label="E-mail"
-            clearable
-            required
-          />
-          <v-text-field
-            v-model="user.password"
-            :readonly="authStore.loading.login"
-            :counter="10"
-            label="Last name"
-            type="password"
-            id="password"
-            required
-            clearable
-          />
-          <v-btn
-            :loading="authStore.loading.login"
-            size="large"
-            type="submit"
-            variant="elevated"
-            block
-          >
-            Sign In
-          </v-btn>
-          <v-btn
-            :readonly="authStore.loading.login"
-            size="large"
-            variant="elevated"
-            block
-            class="mt-5"
-            @click="router.push({ name: 'sign-up' })"
-          >
-            Cadastrar
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="4">
+        <v-text-field
+          id="email"
+          v-model="user.email"
+          :readonly="authStore.loading.login"
+          label="E-mail"
+          clearable
+          required
+        />
+        <v-text-field
+          v-model="user.password"
+          :readonly="authStore.loading.login"
+          :counter="10"
+          label="Last name"
+          type="password"
+          id="password"
+          required
+          clearable
+        />
+        <v-btn
+          :loading="authStore.loading.login"
+          size="large"
+          type="submit"
+          variant="elevated"
+          block
+        >
+          Sign In
+        </v-btn>
+        <v-btn
+          :readonly="authStore.loading.login"
+          size="large"
+          variant="elevated"
+          block
+          class="mt-5"
+          @click="router.push({ name: 'sign-up' })"
+        >
+          Cadastrar
+        </v-btn>
+      </v-col>
+    </v-row>
   </v-form>
 </template>
 
