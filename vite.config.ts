df3fcfdb279bc1ff2110499f0d5497e473ecc263
Henrique Fonseca <henriquefons@hotmail.com>
2025-08-17 11:05:02 -0300

    fix: gh pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/work-guard-frontend/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
