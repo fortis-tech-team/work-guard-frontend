e8f5c35bf0e493e9d1b2d7611f07d1f738020abc
Henrique Fonseca <henriquefons@hotmail.com>
2025-08-20 14:38:02 -0300

    feat: improve Home page, add suggestions feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import router from '@/router';
-import { useWorkPermissionStore } from '@/stores/work-permission';
-import { ref } from 'vue';
+import { useWorkPermissionGenerateStore } from '@/stores/work-permission-generates';
+import { storeToRefs } from 'pinia';
+import { onUnmounted, ref } from 'vue';
 
-const workPermissionStore = useWorkPermissionStore();
+const workPermissionStore = useWorkPermissionGenerateStore();
+const { generationWorkPermission } = storeToRefs(workPermissionStore);
+
+onUnmounted(() => {
+  workPermissionStore.$reset();
+});
 
 const searchModel = ref('');
 const errorStatus = ref<string | null>(null);
 
 function onSearch() {
+  errorStatus.value = null;
+
   workPermissionStore
     .generateWorkPermission(searchModel.value)
     .then((result) => {
@@ -22,6 +30,29 @@ function onSearch() {
     .catch((error) => {
       errorStatus.value = error.message || 'Erro ao buscar a permissão de trabalho.';
     });
+}
+
+const PTSuggestions = [
+  'Içamento e posicionamento de motor elétrico na base do misturador M-500.',
+  'Transferência de ácido sulfúrico do caminhão-tanque para o tanque de armazenamento TQ-301.',
+  'Limpeza interna e inspeção do tanque de armazenamento de água T-101.',
+  'Manutenção em válvula no interior de galeria subterrânea de utilidades.',
+  'Esmerilhamento de tubulação de aço carbono na linha de produção 2.',
+  'Medição de grandezas elétricas no barramento principal do painel geral de baixa tensão.',
+  'Uso de soprador térmico para reparo em lona na área externa.',
+  'Instalação de antena de comunicação na torre de telecomunicações.',
+  'Bloqueio e etiquetagem (LOTO) do motor da bomba P-345 para manutenção mecânica.',
+  'Abertura de vala com 1,5m de profundidade para passagem de eletroduto.',
+];
+function getRandomPTSuggestion(limit: number): string[] {
+  const apiSuggestions = generationWorkPermission.value.error?.suggestions || [];
+  const copyList = apiSuggestions.length ? [...apiSuggestions] : [...PTSuggestions];
+  for (let i = copyList.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copyList[i], copyList[j]] = [copyList[j], copyList[i]];
+  }
+
+  return copyList.slice(0, limit);
 }
 </script>
 
@@ -42,6 +73,24 @@ function onSearch() {
           class="mt-2"
         />
       </v-col>
+      <v-col v-if="false">
+        <h4>Qual a sua tarefa de hoje?</h4>
+        <v-autocomplete
+          :error="!!workPermissionStore.error || !!errorStatus"
+          :messages="workPermissionStore.error || errorStatus || ''"
+          :loading="workPermissionStore.loading.generate"
+          :disabled="workPermissionStore.loading.generate"
+          v-model="searchModel"
+          :items="getRandomPTSuggestion(5)"
+          class="mt-2"
+          menu-icon=""
+          placeholder="Pesquisar permissão de trabalho..."
+          prepend-inner-icon="mdi-magnify"
+          theme="light"
+          variant="outlined"
+          auto-select-first
+        />
+      </v-col>
     </v-row>
 
     <v-row>
@@ -59,13 +108,10 @@ function onSearch() {
     </v-row>
   </v-form>
 
-  <v-col class="mt-6" v-if="!!workPermissionStore.workPermission.error?.suggestions.length">
+  <v-col class="mt-6">
     <h4>Sugestoes:</h4>
     <v-list bg-color="white">
-      <v-list-item
-        v-for="(item, index) in workPermissionStore.workPermission.error?.suggestions"
-        :key="index"
-      >
+      <v-list-item v-for="(item, index) in getRandomPTSuggestion(5)" :key="index">
         <v-list-item-content>
           <v-list-item-title>{{ item }}</v-list-item-title>
         </v-list-item-content>
