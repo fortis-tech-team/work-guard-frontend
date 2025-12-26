7fd76fe5021acf5eb77fb5bdb4116ccf6917ab05
Henrique Fonseca <henriquefons@hotmail.com>
2025-12-26 14:25:55 -0300

    feat: create sections component for show PT details

---
+++ src/components/work-permission-form/SectionContent.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { FormGroup, RiskList, ChecklistGroup, SignatureArea } from './section-types';
+import type { SectionContent } from '@/interfaces/models/WorkPermission';
+
+defineProps<{
+  type: 'form_group' | 'risk_list' | 'checklist_group' | 'signature_area';
+  content: SectionContent;
+}>();
+</script>
+
+<template>
+  <v-row class="work-permissions-content">
+    <component
+      :is="
+        {
+          form_group: FormGroup,
+          risk_list: RiskList,
+          checklist_group: ChecklistGroup,
+          signature_area: SignatureArea,
+        }[type]
+      "
+      :content="content"
+    />
+  </v-row>
+</template>
+
+<style scoped>
+.work-permissions-content :deep(.v-field) {
+  border-radius: 0.5rem !important;
+}
+</style>
---
+++ src/components/work-permission-form/SectionTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-col class="d-flex align-center justify-space-between">
+      <h3>
+        <slot></slot>
+      </h3>
+      <v-btn size="small" prepend-icon="mdi-plus" variant="outlined" rounded="xl">
+        Adicionar item
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+h3 {
+  font-size: 1rem;
+  font-weight: 600;
+  color: #35393b;
+}
+</style>
